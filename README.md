e1687280cdb07dce0aacec5fd170f361afe393df
simonreddy2001 <77497059+simonreddy2001@users.noreply.github.com>
2021-03-04 13:59:57 +0100

    updated mobile meadia queries

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NiceVeryNice
 its a web page for simple code with HTML and CSS.
+
+it has used flexbox in css.
--- learnJS.js
+++ learnJS.js
@@ -1,3 +1,7 @@
 function area (length, width){
 return length*width;
 }
+
+function add(a,b){
+    return a+b;
+}
